dd991493da3d17ad18390bb09e49f724aec93c72
edancy <elizabeth.dancy@sheridancollege.ca>
2023-05-10 10:12:04 -0400

    first commit of code

---
+++ src/studentdemo4/StudentDemo4.java
@@ -0,0 +1,17 @@
+
+package studentdemo4;
+
+/**
+ *
+ * @author dancye
+ */
+public class StudentDemo4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
